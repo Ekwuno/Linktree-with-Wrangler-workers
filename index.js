35676c4f8410ad91091c4cd4027420a927b6339e
Ekwuno Obinna <obinnacodes@gmail.com>
2021-02-19 22:24:15 +0400

    displayed svg icons

--- index.js
+++ index.js
@@ -63,25 +63,32 @@ class BodyTransformer {
 //add svgs here
 var svgs = [
   {
-    svg: 'twitter svg',
-    url:
-      'https://res.cloudinary.com/ekwuno/image/upload/v1613611969/twitter.svg',
+    name: '/sVG/twitter svg 2.0.svg',
+    link: 'https://twitter.com/Obinnaspeaks',
+    svg:
+      '<svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Twitter icon</title><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"/></svg>',
   },
   {
-    svg: 'LinkedIn svg',
-    url:
-      'https://res.cloudinary.com/ekwuno/image/upload/v1613612188/linkedin.svg',
+    name: 'LinkedIn svg',
+    link: 'https://www.linkedin.com/in/ekwunoobinna/',
+    svg:
+      '<svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn icon</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>',
   },
-  { svg: 'web svg', url: 'https://linkurl' },
+  {
+    name: 'web svg',
+    link: 'https://www.obinnaspeaks.dev/',
+    svg:
+      '<svg role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Google Scholar icon</title><path d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"/></svg>',
+  },
 ]
 class SocialTransformer {
-  async element(element) {
-    element.removeAttribute('style')
+  constructor(svgs) {
+    this.svgs = svgs
+  }
 
+  async element(element) {
     for (let svg of svgs) {
-      element.append(`<a href="${svg.url}"><svg>${svg.svg}</svg></a>`, {
-        html: true,
-      })
+      element.append(`<a href="${svg.link}"> ${svg.svg} </a>`, { html: true })
     }
   }
 }
@@ -113,7 +120,8 @@ async function handleRequestForOthers(request) {
       ),
     )
     .on('h1#name', new TextTransformer('obinnacodes'))
-    .on('div#social', new SocialTransformer())
+    .on('div#social', new SocialTransformer(svgs))
+    .on('div#social', new ProfileTransformer())
     .on('title', new TextTransformer('Obinna Ekwuno'))
     .on('body', new AttributeTransformer('class', 'bg-red-400'))
     .transform(html)
